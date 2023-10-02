0a79bb0cbfe3c8f8d040d572e4fc8ceef6ddc47c
Gabriel Chaves <gabriel11schaves@gmail.com>
2023-10-02 18:31:39 -0300

    Initial commit

---
+++ src/main/java/com/SimpleCarAPI/SimpleCarApi_RemakeChallengeUOL/SimpleCarApiRemakeChallengeUolApplication.java
@@ -0,0 +1,13 @@
+package com.SimpleCarAPI.SimpleCarApi_RemakeChallengeUOL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCarApiRemakeChallengeUolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCarApiRemakeChallengeUolApplication.class, args);
+	}
+
+}
